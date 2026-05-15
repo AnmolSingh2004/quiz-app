0ae3cc56d3480d42cecafbfcb10475c49cfb119a
AnmolSingh2004 <singa30@farmingdale.edu>
2026-05-14 21:50:26 -0400

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -3,14 +3,16 @@
 $host = "switchback.proxy.rlwy.net";
 $db   = "railway";
 $user = "root";
-$pass = "PAYWpYNJGgiPIVpYybklPQYNNCTzMSHK";
+$pass = "PAYWpYNJGgiPIVpYybklPQYNNCtzMSHk";
 $port = "48473";
-
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
 try {
 
-    $pdo = new PDO($dsn, $user, $pass);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass
+    );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
