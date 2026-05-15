3c62bcede6050c480b65744209115d4785548609
AnmolSingh2004 <singa30@farmingdale.edu>
2026-05-14 21:44:58 -0400

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -3,12 +3,13 @@
 $host = "switchback.proxy.rlwy.net";
 $db   = "railway";
 $user = "root";
-$pass = "PASTE_YOUR_RAILWAY_PASSWORD_HERE";
+$pass = "PAYWpYNJGgiPIVpYybklPQYNNCTzMSHK";
 $port = "48473";
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
 try {
+
     $pdo = new PDO($dsn, $user, $pass);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -18,4 +19,5 @@ try {
     die("Database connection failed: " . $e->getMessage());
 
 }
+
 ?>
